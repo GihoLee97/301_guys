01f682e68da2176dac0dc84d029a85222e56ea27
GihoLee97 <97leegh@snu.ac.kr>
2021-01-27 18:04:20 +0900

    2020-01-27 linking complished

--- app/src/main/java/com/example/myapplication/Profile.kt
+++ app/src/main/java/com/example/myapplication/Profile.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val nickname_btn = findViewById<Button>(R.id.nickname_btn)
+        nickname_btn.setOnClickListener {
+            val layoutInflater: LayoutInflater = getLayoutInflater()
+            val builder = AlertDialog.Builder(this)
+            val dialogview = layoutInflater.inflate(R.layout.nickname_dialog,null)
+            builder.setView(dialogview).show()
+        }
+
     }
 }
