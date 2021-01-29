d682a41e6c3c76b19a7f72b85ebb8d3144b9f927
Jihong-Kim97 <hong99z@snu.ac.kr>
2021-01-29 23:07:48 +0900

    202101292307

---
+++ app/src/main/java/com/example/myapplication/ProflieDB.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities =  [Profile::class], version =  1)
+abstract class ProflieDB: RoomDatabase() {
+    abstract  fun profileDao(): ProfileDao
+
+    companion object{
+        private var INSTANCE : ProflieDB? =null
+
+        fun getInstace(context: Context): ProflieDB?{
+            if (INSTANCE == null){
+                synchronized(ProflieDB::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                        ProflieDB::class.java,
+                        "playlist.db")
+                        .allowMainThreadQueries().fallbackToDestructiveMigration()
+                        .build()
+
+
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyINSTACE(){
+            INSTANCE = null
+        }
+    }
+}
