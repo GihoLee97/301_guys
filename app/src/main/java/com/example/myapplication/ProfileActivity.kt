0bc745135dd1dcb6a485723cb31ab0a8961f98c3
GihoLee97 <97leegh@snu.ac.kr>
2021-02-01 18:19:46 +0900

    2021-02-01 계정관리 레이아웃 추가 및 링킹 (google/kakao 연동X)

--- app/src/main/java/com/example/myapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/ProfileActivity.kt
@@ -1,21 +1,21 @@
 package com.example.myapplication
 
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.data.Profile
 import com.example.myapplication.data.ProflieDB
-import org.w3c.dom.Text
 
 class ProfileActivity : AppCompatActivity() {
     //receive profile room data
     private var profileDb: ProflieDB? = null
     private var profileList = mutableListOf<Profile>()
-    private lateinit var nickname_text: TextView
+    private lateinit var nickname_textView: TextView
+    private lateinit var accountManagement_btn: Button
+    private lateinit var nickname_btn: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +23,16 @@ class ProfileActivity : AppCompatActivity() {
 
         profileDb = ProflieDB.getInstace(this)
 
-        nickname_text = findViewById<TextView>(R.id.nickName_text)
+        nickname_textView = findViewById(R.id.nickName_text)
+        nickname_btn = findViewById(R.id.nickname_btn)
+        accountManagement_btn = findViewById(R.id.accountManagement_btn)
 
         val startRunnable = Runnable {
             profileList = profileDb?.profileDao()?.getAll()!!
         }
 
         if (profileDb?.profileDao()?.getAll().isNullOrEmpty()) { // PreSet the setting in the case of first running
-            nickname_text.text = "닉네임을 아직 정하지 않았습니다."
+            nickname_textView.text = "닉네임을 아직 정하지 않았습니다."
             // preinsert profile for call_dialog_nick which just update the database
             val setRunnable = Runnable {
                 val newProfile = Profile(1, "닉네임을 아직 정하지 않았습니다.", 0, "", 1, "")
@@ -41,15 +43,19 @@ class ProfileActivity : AppCompatActivity() {
             // update nickname
             call_dialog_nick()
         } else { // recall database
-            nickname_text.text = profileDb?.profileDao()?.getNickname()
+            nickname_textView.text = profileDb?.profileDao()?.getNickname()
         }
 
         val startThread = Thread(startRunnable)
         startThread.start()
 
-        val nickname_btn = findViewById<Button>(R.id.nickname_btn)
         nickname_btn.setOnClickListener {
             call_dialog_nick()
+        }
+
+        accountManagement_btn.setOnClickListener {
+            val intent = Intent(this, AccountManagementActivity::class.java) //Main으로 이동
+            startActivity(intent)
         }
     }
 
@@ -57,7 +63,7 @@ class ProfileActivity : AppCompatActivity() {
     private fun call_dialog_nick() {
         val dlg_nick = Dialog_nick(this)
         dlg_nick.start(profileDb)
-        nickname_text.text = profileDb?.profileDao()?.getNickname() //TODO!!!!!
+        nickname_textView.text = profileDb?.profileDao()?.getNickname() //TODO!!!!!
     }
 }
 
