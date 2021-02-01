a3aaceb970ba29504d14386d055ebfa0c4ec635d
GihoLee97 <97leegh@snu.ac.kr>
2021-02-01 17:26:09 +0900

    2021-02-01 이미 로그인 한 경우 initial Activity passing (kakao 수정중)

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -3,12 +3,15 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
+import com.google.gson.internal.reflect.ReflectionAccessor
+import com.kakao.sdk.auth.LoginClient
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.user.UserApiClient
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var mAuth : FirebaseAuth
@@ -41,9 +44,23 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun isKakaoAuthChecked(): Boolean{
-        var alreadyKakaoAuth = false
+        var alreadyKakaoAuth = false//TODO!!!!!!!!!!!!!!!!!!!!(From dataBase)
         if(alreadyKakaoAuth) {// available access authority
-            val intent = Intent(this, MainActivity::class.java) //Main으로 이동
+
+//            val TAG = "SplashActivity"
+            // 로그인 공통 callback 구성
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null || token == null) alreadyKakaoAuth = false//TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                else Toast.makeText(this, "pass!!!!", Toast.LENGTH_LONG).show()
+            }
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (LoginClient.instance.isKakaoTalkLoginAvailable(this)) {
+                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
+            } else {
+                LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+
+            val intent = Intent(this, MainActivity::class.java) // Main으로 이동
             startActivity(intent)
             return true
         }
