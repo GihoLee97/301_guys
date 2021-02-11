ee2facabe475027bbb20ac262b0d6b4457d1cbe9
JEONWSEONGWON <s2jsw2s0121@gmail.com>
2021-02-11 17:31:15 +0900

    2021/02/11 전성원 1. MainActivity에 랭킹 버튼 구현. 서버에서 정보를 받아와서 보유 현금 순서대로 10등까지 보여줌

--- app/src/main/java/com/guys_from_301/stock_game/MainActivity.kt
+++ app/src/main/java/com/guys_from_301/stock_game/MainActivity.kt
@@ -8,6 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.guys_from_301.stock_game.data.GameSetDB
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.guys_from_301.stock_game.retrofit.RetrofitRanking
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+var rank1_nick :String = ""; var rank1_money :String = ""; var rank2_nick :String = ""; var rank2_money :String = ""
+var rank3_nick :String = ""; var rank3_money :String = ""; var rank4_nick :String = ""; var rank4_money :String = ""
+var rank5_nick :String = ""; var rank5_money :String = ""; var rank6_nick :String = ""; var rank6_money :String = ""
+var rank7_nick :String = ""; var rank7_money :String = ""; var rank8_nick :String = ""; var rank8_money :String = ""
+var rank9_nick :String = ""; var rank9_money :String = ""; var rank10_nick :String = ""; var rank10_money :String = ""
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btn_setting : Button
     private lateinit var btn_game : Button
     private lateinit var btn_market : Button
+    private lateinit var btn_ranking: Button
+
     lateinit var mAdView : AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +45,7 @@ class MainActivity : AppCompatActivity() {
         btn_setting = findViewById(R.id.btn_setting)
         btn_game =  findViewById(R.id.btn_game)
         btn_market = findViewById(R.id.btn_market)
+        btn_ranking = findViewById(R.id.btn_ranking)
 
         btn_game.isEnabled = false // 로딩 미완료 상태일 때 게임 버튼 비활성화
 
@@ -61,6 +77,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,MarketActivity::class.java)
             startActivity(intent)
         }
+        btn_ranking.setOnClickListener{
+            val intent = Intent(this, RankingActivity::class.java)
+            startActivity(intent)
+        }
 
         while (true) {
             if (loadcomp) {
@@ -70,9 +90,13 @@ class MainActivity : AppCompatActivity() {
             }
             Thread.sleep(50)
         }
-
+        ranking("a")
     }
 
+    override fun onStart() {
+        super.onStart()
+        ranking("a")
+    }
     // 두번 누르면 종료되는 코드
     var time3: Long = 0
     override fun onBackPressed() {
@@ -88,5 +112,43 @@ class MainActivity : AppCompatActivity() {
             time3 = time1
             Toast.makeText(applicationContext, "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    // 랭킹 받아오는 코드
+
+    fun ranking(u_id: String) {
+        var funranking: RetrofitRanking? = null
+        val url = "http://stockgame.dothome.co.kr/test/ranking.php/"
+        var gson: Gson = GsonBuilder()
+                .setLenient()
+                .create()
+        //creating retrofit object
+        var retrofit =
+                Retrofit.Builder()
+                        .baseUrl(url)
+                        .addConverterFactory(GsonConverterFactory.create(gson))
+                        .build()
+        //creating our api
+        funranking = retrofit.create(RetrofitRanking::class.java)
+        funranking.funranking(u_id).enqueue(object : Callback<Array<String>> {
+            override fun onFailure(call: Call<Array<String>>, t: Throwable) {
+                //Toast.makeText(this@InitialActivity, "아이디나 비밀번호가 맞지 않습니다.", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(call: Call<Array<String>>, response: retrofit2.Response<Array<String>>) {
+                if (response.isSuccessful && response.body() != null) {
+                    rank1_nick = response.body()!![0]; rank1_money = response.body()!![10]
+                    rank2_nick = response.body()!![1]; rank2_money = response.body()!![11]
+                    rank3_nick = response.body()!![2]; rank3_money = response.body()!![12]
+                    rank4_nick = response.body()!![3]; rank4_money = response.body()!![13]
+                    rank5_nick = response.body()!![4]; rank5_money = response.body()!![14]
+                    rank6_nick = response.body()!![5]; rank6_money = response.body()!![15]
+                    rank7_nick = response.body()!![6]; rank7_money = response.body()!![16]
+                    rank8_nick = response.body()!![7]; rank8_money = response.body()!![17]
+                    rank9_nick = response.body()!![8]; rank9_money = response.body()!![18]
+                    rank10_nick = response.body()!![9]; rank10_money = response.body()!![19]
+                }
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/guys_from_301/stock_game/retrofit/RetrofitRanking.kt
@@ -0,0 +1,23 @@
+package com.guys_from_301.stock_game.retrofit
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.guys_from_301.stock_game.RetrofitUpdate
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface RetrofitRanking {
+    @FormUrlEncoded
+    @POST("/test/ranking.php/")
+    fun funranking(
+            @Field("u_id") u_id : String
+    ): Call<Array<String>>
+}
+
+
+
