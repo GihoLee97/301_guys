7a7ba10745a218f7df9c0734eccffce9b66d1bd4
JEONSEONGWON <s2jsw2s0121@gmail.com>
2021-02-25 20:43:33 +0900

    21/02/25 전성원 2 프로필 테두리 색깔 입힘

--- app/src/main/java/com/guys_from_301/stock_game/FragmentProfile.kt
+++ app/src/main/java/com/guys_from_301/stock_game/FragmentProfile.kt
@@ -2,6 +2,7 @@ package com.guys_from_301.stock_game
 
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.*
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,7 +12,11 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.BitmapImageViewTarget
 import com.guys_from_301.stock_game.data.ProfileDB
 import com.kakao.sdk.user.UserApiClient
 import org.w3c.dom.Text
@@ -56,10 +61,12 @@ class FragmentProfile : Fragment() {
                 if (error!=null)
                     Toast.makeText(_MainActivity,"사용자 정보 요청 실패(카카오)", Toast.LENGTH_SHORT)
                 else if (user!=null) {
-                    Glide.with(_MainActivity!!).load(user?.kakaoAccount?.profile?.thumbnailImageUrl).circleCrop().into(v.findViewById<ImageView>(R.id.iv_my_image))
+                    v.findViewById<ImageView>(R.id.iv_my_image).loadCircularImage(user?.kakaoAccount?.profile?.thumbnailImageUrl, 5F, Color.parseColor("#F4730B"))
+//                    Glide.with(_MainActivity!!).load(user?.kakaoAccount?.profile?.thumbnailImageUrl).circleCrop().into(v.findViewById<ImageView>(R.id.iv_my_image))
                 }
             }
         }
+//        v.findViewById<ImageView>(R.id.iv_my_image).
         v.findViewById<TextView>(R.id.tv_nickname_change).setOnClickListener{
             val dlg = Dialog_nick(_MainActivity!!, false, profileActivityViewModel)
             dlg.start(profileDb)
@@ -108,15 +115,78 @@ class FragmentProfile : Fragment() {
 //    }
 
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment2.
-         */
+    fun <T> ImageView.loadCircularImage(
+            model: T,
+            borderSize: Float = 0F,
+            borderColor: Int = Color.WHITE
+    ) {
+        Glide.with(context)
+                .asBitmap()
+                .load(model)
+                .apply(RequestOptions.circleCropTransform())
+                .into(object : BitmapImageViewTarget(this) {
+                    override fun setResource(resource: Bitmap?) {
+                        setImageDrawable(
+                                resource?.run {
+                                    RoundedBitmapDrawableFactory.create(
+                                            resources,
+                                            if (borderSize > 0) {
+                                                createBitmapWithBorder(borderSize, borderColor)
+                                            } else {
+                                                this
+                                            }
+                                    ).apply {
+                                        isCircular = true
+                                    }
+                                }
+                        )
+                    }
+                })
+    }
 
+    /**
+     * Create a new bordered bitmap with the specified borderSize and borderColor
+     *
+     * @param borderSize - The border size in pixel
+     * @param borderColor - The border color
+     * @return A new bordered bitmap with the specified borderSize and borderColor
+     */
+    private fun Bitmap.createBitmapWithBorder(borderSize: Float, borderColor: Int): Bitmap {
+        val borderOffset = (borderSize * 2).toInt()
+        val halfWidth = width / 2
+        val halfHeight = height / 2
+        val circleRadius = Math.min(halfWidth, halfHeight).toFloat()
+        val newBitmap = Bitmap.createBitmap(
+                width + borderOffset,
+                height + borderOffset,
+                Bitmap.Config.ARGB_8888
+        )
+
+        // Center coordinates of the image
+        val centerX = halfWidth + borderSize
+        val centerY = halfHeight + borderSize
+
+        val paint = Paint()
+        val canvas = Canvas(newBitmap).apply {
+            // Set transparent initial area
+            drawARGB(0, 0, 0, 0)
+        }
+
+        // Draw the transparent initial area
+        paint.isAntiAlias = true
+        paint.style = Paint.Style.FILL
+        canvas.drawCircle(centerX, centerY, circleRadius, paint)
+
+        // Draw the image
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(this, borderSize, borderSize, paint)
+
+        // Draw the createBitmapWithBorder
+        paint.xfermode = null
+        paint.style = Paint.Style.STROKE
+        paint.color = borderColor
+        paint.strokeWidth = borderSize
+        canvas.drawCircle(centerX, centerY, circleRadius, paint)
+        return newBitmap
     }
 }
