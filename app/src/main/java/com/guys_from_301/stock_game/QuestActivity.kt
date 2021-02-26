2990bc8282fd4ea514b236d5b10644a430592516
JEONSEONGWON <s2jsw2s0121@gmail.com>
2021-02-26 14:39:56 +0900

    21/02/26 전성원 1. 지홍이 작업(도전과제) 반영. 2. 자잘한 xml 수정

--- app/src/main/java/com/guys_from_301/stock_game/QuestActivity.kt
+++ app/src/main/java/com/guys_from_301/stock_game/QuestActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,12 +14,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.guys_from_301.stock_game.data.ProfileDB
 import com.guys_from_301.stock_game.data.Quest
 import com.guys_from_301.stock_game.data.QuestDB
+import kotlin.math.exp
 
 class QuestActivity : AppCompatActivity() {
 
     private var questDb: QuestDB? = null
     private var profileDb: ProfileDB? = null
     private var questList = listOf<Quest>()
+    private var achievementList = listOf<Int>()
+    private var questInGameProfitRateList = listOf<Quest>()
+    private var questRelativeProfitRateList = listOf<Quest>()
+    private var questCumulativeList = listOf<Quest>()
+    private var questPlayedGameList = listOf<Quest>()
+    private var questInviteList = listOf<Quest>()
     private lateinit var tv_level : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,23 +36,62 @@ class QuestActivity : AppCompatActivity() {
         questDb = QuestDB.getInstance(this)
         profileDb = ProfileDB.getInstace(this)
         tv_level = findViewById(R.id.tv_level)
-        tv_level.text = "   레벨 "+ profileDb?.profileDao()?.getLevel()!!
-        findViewById<TextView>(R.id.tv_go_back).setOnClickListener{
-            onBackPressed()
-        }
-        var mAdapter = QuestAdapter(this, questList)
+        tv_level.text = "레벨 "+ profileDb?.profileDao()?.getLevel()!!
+        var mAdapter = MyQuestAdapter(this, questList, achievementList)
         val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
         val qRecyclerView = findViewById<RecyclerView>(R.id.qRecyclerView)
-        val gridLayoutManager = GridLayoutManager(applicationContext, 3)
+        val gridLayoutManager = GridLayoutManager(applicationContext, 2)
+        var profitAchievement = 0
+        var relativeProfitAchievement = 0
+        var surplusAchievement = 0
+        var exprienceAchievement = 0
+        var invitationAchievemnet = 0
+        findViewById<ImageButton>(R.id.ib_go_back).setOnClickListener{
+            onBackPressed()
+        }
         qRecyclerView.layoutManager = gridLayoutManager
         val r = Runnable {
             try{
-                questList = questDb?.questDao()?.getAll()!!
-                mAdapter = QuestAdapter(this, questList)
+                //성공한 도전과제 받아오기
+                questInGameProfitRateList = questDb?.questDao()?.getQuestToAdapterByTheme("수익률")!!
+                questRelativeProfitRateList = questDb?.questDao()?.getQuestToAdapterByTheme("시장대비 수익률")!!
+                questCumulativeList = questDb?.questDao()?.getQuestToAdapterByTheme("연속 흑자")!!
+                questPlayedGameList = questDb?.questDao()?.getQuestToAdapterByTheme("투자 경험")!!
+                questInviteList = questDb?.questDao()?.getQuestToAdapterByTheme("초대하기")!!
+                profitAchievement = 100 - questInGameProfitRateList.size * 10
+                relativeProfitAchievement = 100 - questRelativeProfitRateList.size * 20
+                surplusAchievement = 100 - questCumulativeList.size * 20
+                exprienceAchievement = 100 - questPlayedGameList.size * 20
+                invitationAchievemnet = 100 - questInviteList.size * 100
+                if (questInGameProfitRateList.isEmpty()) {
+                    questInGameProfitRateList = listOf(Quest(0, "수익률", "투자왕", 1))
+                    profitAchievement = 100
+                }
+                if (questRelativeProfitRateList.isEmpty()) {
+                    questRelativeProfitRateList = listOf(Quest(0, "시장대비 수익률", "시장을 지배하는 투자자", 1))
+                    relativeProfitAchievement = 100
+                }
+                if (questCumulativeList.isEmpty()) {
+                    questCumulativeList = listOf(Quest(0, "연속 흑자", "절대 잃는 법이 없는 투자자", 1))
+                    surplusAchievement = 100
+                }
+                if (questPlayedGameList.isEmpty()) {
+                    questPlayedGameList = listOf(Quest(0, "투자 경험", "노련한 투자자", 1))
+                    exprienceAchievement = 100
+                }
+                if (questInviteList.isEmpty()) {
+                    questInviteList = listOf(Quest(0, "초대하기", "인맥왕", 1))
+                    invitationAchievemnet = 100
+                }
+
+                questList = listOf(questInGameProfitRateList.first(), questRelativeProfitRateList.first(), questCumulativeList.first(), questPlayedGameList.first(), questInviteList.first())
+                achievementList = listOf(profitAchievement,relativeProfitAchievement,surplusAchievement,exprienceAchievement,invitationAchievemnet)
+
+                mAdapter = MyQuestAdapter(this, questList, achievementList)
                 mAdapter.notifyDataSetChanged()
 
                 qRecyclerView.adapter = mAdapter
-                val manager = GridLayoutManager(applicationContext, 3)
+                val manager = GridLayoutManager(applicationContext, 2)
                 qRecyclerView.setHasFixedSize(true)
                 manager.reverseLayout = true
                 manager.stackFromEnd = true
